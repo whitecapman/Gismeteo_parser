80333f8facc447a86834810dfb089cfaabfcc7a9
frame <arsenepiskoposov@gmail.com>
2026-07-31 22:05:18 +0500

    Update exporter.py

--- exporter.py
+++ exporter.py
@@ -6,7 +6,7 @@ def save_to_csv(forecast_data, city_name, filename=None): # информация
         cdatetime=datetime.now().strftime("%Y-%m-%d")
         filename=f"weather_{city_name}_{cdatetime}.csv" # даём название файлу
     with open(filename, "w", newline='', encoding='utf_8') as f: # запись 
-        writer = csv.DictWriter(f, fieldnames=["дата", "состояние", "температура-max", "температура-min"]) # даём заголовки(такие-же что и в словаре)
+        writer = csv.DictWriter(f, fieldnames=["дата", "состояние", "температура-max", "температура-min"]) # даём заголовки (те же что и в словаре)
         writer.writeheader()
         writer.writerows(forecast_data)
     return filename
